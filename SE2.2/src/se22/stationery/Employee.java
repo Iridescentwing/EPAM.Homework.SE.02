e5ba9bd856facb8059003dc7d46b17f71fc68c3c
Екатерина <ekaterinarogozhina@gmail.com>
2016-03-19 21:11:53 +0300

    Added displaying set of stationary for employee.

--- SE2.2/src/se22/stationery/Employee.java
+++ SE2.2/src/se22/stationery/Employee.java
@@ -30,4 +30,8 @@ public class Employee {
         return sum;
     }
 
+    public ArrayList <Stationery> getStationeryList () {
+        return stationeryList;
+    }
+
 }
--- SE2.2/src/se22/stationery/Main.java
+++ SE2.2/src/se22/stationery/Main.java
@@ -1,5 +1,7 @@
 package se22.stationery;
 
+import java.util.ArrayList;
+
 /**
  * Created by Ekaterina on 19.03.16.
  */
@@ -13,26 +15,44 @@ public class Main {
         employee1.employeeID=8342;
 
         Stationery pen1= new Stationery();
-        pen1.price=1;
+        pen1.price=1.9;
         pen1.title="Pen";
 
         Stationery pencil1=new Stationery();
-        pencil1.price=2;
+        pencil1.price=1.5;
         pencil1.title="Pencil";
 
         Stationery eraser = new Stationery ();
-        eraser.price=7;
+        eraser.price=0.7;
         eraser.title="Eraser";
 
         Stationery scissors = new Stationery();
-        scissors.price = 1;
+        scissors.price = 1.7;
         scissors.title="Scissors";
 
         employee1.addStationery(pencil1);
         employee1.addStationery(pen1);
         employee1.addStationery(scissors);
 
-        System.out.println(employee1.getSum());
+
+
+        System.out.println("Employee: " + employee1.name + ", ID: " +employee1.employeeID+ ". Total stationery cost: $" +employee1.getSum() + ".");
+
+
+        System.out.print ("Employee: " + employee1.name + ", ID: " +employee1.employeeID + ". Stationary: ");
+
+
+        ArrayList<Stationery> employee1StationeryList = employee1.getStationeryList();
+        int n=employee1StationeryList.size();
+
+        for (int i=0; i<n; i++) {
+            System.out.print (employee1StationeryList.get(i).title + " ($"+employee1StationeryList.get(i).price+")");
+            if (i == n-1) {
+                System.out.println (".");
+            } else
+                System.out.print (", ");
+        }
+
 
 
 
